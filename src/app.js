d552c6058f1e8a7e0fe06fd5cf14ca418eaf88ed
Nhat Dinh <nhatdinh@Nhats-MacBook-Pro.local>
2021-02-12 14:16:36 -0800

    add more code no tests

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ const dayOfTheMonth = (date = new Date()) => {
     return days[date.getDay()];
 }
 
+const dayOfTheYear = (date = new Date()) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return days[date.getDay()];
+}
+
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
